5cd7e9b4064223d2003d4a5acf27c8e8c18d8113
123 <123>
2019-11-13 20:19:22 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello, world!");
         System.out.println("Some text");
         System.out.println("asfsdf");
+        System.out.println("asdfsdf");
     }
 }
